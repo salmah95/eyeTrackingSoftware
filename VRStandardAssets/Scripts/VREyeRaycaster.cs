cd09fe6a68eb11b5fb17d50c318a6b59d9d5e978
Salma Hussein <salma.hussein.95@hotmail.com>
2018-08-29 14:52:58 +0100

    updated eye raycaster

--- VRStandardAssets/Scripts/VREyeRaycaster.cs
+++ VRStandardAssets/Scripts/VREyeRaycaster.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 namespace VRStandardAssets.Utils
 {
+
     // In order to interact with objects in the scene
     // this class casts a ray into the scene and if it finds
     // a VRInteractiveItem it exposes it for other classes to use.
@@ -10,7 +11,7 @@ namespace VRStandardAssets.Utils
     public class VREyeRaycaster : MonoBehaviour
     {
         public event Action<RaycastHit> OnRaycasthit;                   // This event is called every frame that the user's gaze is over a collider.
-
+        int count = 0;
 
         [SerializeField] private Transform m_Camera;
         [SerializeField] private LayerMask m_ExclusionLayers;           // Layers to exclude from the raycast.
@@ -21,18 +22,16 @@ namespace VRStandardAssets.Utils
         [SerializeField] private float m_DebugRayDuration = 1f;         // How long the Debug ray will remain visible.
         [SerializeField] private float m_RayLength = 500f;              // How far into the scene the ray is cast.
 
-        
+
         private VRInteractiveItem m_CurrentInteractible;                //The current interactive item
         private VRInteractiveItem m_LastInteractible;                   //The last interactive item
-
-
+        [SerializeField] private GameObject[] heatTex;
         // Utility for other classes to get the current interactive item
         public VRInteractiveItem CurrentInteractible
         {
             get { return m_CurrentInteractible; }
         }
-
-        
+ 
         private void OnEnable()
         {
             m_VrInput.OnClick += HandleClick;
@@ -54,8 +53,10 @@ namespace VRStandardAssets.Utils
         private void Update()
         {
             EyeRaycast();
+            count++;
         }
-
+        
+        
       
         private void EyeRaycast()
         {
@@ -68,14 +69,36 @@ namespace VRStandardAssets.Utils
             // Create a ray that points forwards from the camera.
             Ray ray = new Ray(m_Camera.position, m_Camera.forward);
             RaycastHit hit;
-            
+           
             // Do the raycast forweards to see if we hit an interactive item
             if (Physics.Raycast(ray, out hit, m_RayLength, ~m_ExclusionLayers))
             {
                 VRInteractiveItem interactible = hit.collider.GetComponent<VRInteractiveItem>(); //attempt to get the VRInteractiveItem on the hit object
-                m_CurrentInteractible = interactible;
+                m_CurrentInteractible = interactible; //make the item interactable 
 
-                // If we hit an interactive item and it's not the same as the last interactive item, then call Over
+                //create a point on the area in which there is an intersection between the ray and the object 
+                GameObject heat = Instantiate(heatTex[0], hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal)) as GameObject;
+
+               
+                
+               
+
+                 if(Input.GetKeyDown("s"))
+                  {
+                     GameObject[] heat2 =GameObject.FindGameObjectsWithTag("EYE");
+                     for(int i = 0; i < 500000000; i++)
+                     heat2[i].GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(1.0f, 1.0f, 1.0f, 1.0f));
+                  }
+             /* if (Input.GetKeyDown("t"))
+              {
+                  GameObject[] heat2 = GameObject.FindGameObjectsWithTag("EYE");
+                  for (int i = 0; i < 500000000; i++)
+                      heat2[i].GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(1.0f, 0.0f, 0.0f, 0.0f));
+              }*/
+              
+        
+
+                //If we hit an interactive item and it's not the same as the last interactive item, then call Over
                 if (interactible && interactible != m_LastInteractible)
                     interactible.Over(); 
 
@@ -91,6 +114,11 @@ namespace VRStandardAssets.Utils
 
                 if (OnRaycasthit != null)
                     OnRaycasthit(hit);
+
+                
+               
+
+                //Debug.Log("here");
             }
             else
             {
@@ -101,6 +129,7 @@ namespace VRStandardAssets.Utils
                 // Position the reticle at default distance.
                 if (m_Reticle)
                     m_Reticle.SetPosition();
+                 
             }
         }
 
